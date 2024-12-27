b9163209692889230c180f099b2c6299f36cd629
Arnav Khurma <arnavkhurma@Arnavs-MacBook-Pro.local>
2024-12-28 00:11:49 +0530

    updated data for storage/imports/exports

--- EIA/get_request.py
+++ EIA/get_request.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import os
 from dotenv import load_dotenv
 import pandas as pd
@@ -15,8 +14,19 @@ url_database = [
     ['Gross Withdrawals and Production', 'https://api.eia.gov/v2/natural-gas/prod/sum/data/?', 'monthly'],
     ['Natural Gas Plant Processing', 'https://api.eia.gov/v2/natural-gas/prod/pp/data/?', 'monthly'],
     ['Natural Gas Plant Liquids Production', 'https://api.eia.gov/v2/natural-gas/prod/ngpl/data/?', 'annual'],
-    ['Offshore Gross Withdrawals of Natural Gas', 'https://api.eia.gov/v2/natural-gas/prod/off/data/?', 'monthly']
+    ['Offshore Gross Withdrawals of Natural Gas', 'https://api.eia.gov/v2/natural-gas/prod/off/data/?', 'monthly'],
+    ['Weekly Working Gas in Underground Storage', 'https://api.eia.gov/v2/natural-gas/stor/wkly/data/?', 'weekly'],
+    ['Underground Natural Gas Storage By All Operators', 'https://api.eia.gov/v2/natural-gas/stor/sum/data/?', 'monthly'],
+    ['U.S Underground Natural Gas Storage By Storage Type', 'https://api.eia.gov/v2/natural-gas/stor/type/data/?', 'monthly'],
+    ['Underground Natural Gas Storage Capacity', 'https://api.eia.gov/v2/natural-gas/stor/cap/data/?', 'monthly'],
+    ['Liquefied Natural Gas Additions to and Withdrawals from Storage', 'https://api.eia.gov/v2/natural-gas/stor/lng/data/?', 'annual'],
+    ['Number of Natural Gas Consumers', 'https://api.eia.gov/v2/natural-gas/cons/num/data/?', 'annual'],
+    ['Natural Gas Consumers By End Use', 'https://api.eia.gov/v2/natural-gas/cons/sum/data/?', 'monthly'],
+    ['U.S. Natural Gas Imports & Exports by State', 'https://api.eia.gov/v2/natural-gas/move/state/data/?', 'monthly'],
+    ['U.S Natural Gas Imports By Country', 'https://api.eia.gov/v2/natural-gas/move/impc/data/?', 'monthly'],
+    ['U.S Natural Gas Exports and Re-exports By Country', 'https://api.eia.gov/v2/natural-gas/move/expc/data/?', 'monthly']
 ]
+
 
 def reload_data(url_database):
     for item in url_database:
